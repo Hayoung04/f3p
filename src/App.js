ed7a483e21fb480e9730a3200c837f6b452b5921
sungyu0309 <tjsrb439@handong.ac.kr>
2024-01-28 00:00:35 +0900

    add : category 파일 추가 및 chore : 파일 routes/basic 이동

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import BaConTopic from "./routes/BaConTopic";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Romantic from "./routes/romantic/Romantic";
+import BaConTopic from "./routes/basic/BaConTopic";
+import Romantic from "./routes/basic/Romantic";
+import Relationship from "./routes/basic/Relationship";
+import Icebreaking from "./routes/basic/Icebreaking";
 
 function App() {
   return (
-    <div>
+    <div id="conversation">
       <Router>
         <Routes>
           <Route path="/" element={<BaConTopic />}></Route>
           <Route path="/romantic" element={<Romantic />}></Route>
-          <Route path="/relationship"></Route>
-          <Route path="/icebreaking"></Route>
+          <Route path="/relationship" element={<Relationship />}></Route>
+          <Route path="/icebreaking" element={<Icebreaking />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/routes/basic/Relationship.js
@@ -0,0 +1,5 @@
+function Relationship() {
+  return;
+}
+
+export default Relationship;
