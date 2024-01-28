fa9d0686aee8130815d6770745102d7927bcd8a4
sungyu0309 <tjsrb439@handong.ac.kr>
2024-01-29 03:49:13 +0900

    style : 대화 주제 추천 페이지 스타일 추가 및 이미지 추가

--- src/routes/conver_function1/BaConTopic.js
+++ src/routes/conver_function1/BaConTopic.js
@@ -18,26 +18,35 @@ function BaConTopic() {
   };
   return (
     <div id="basic">
-      <h1>F3P</h1>
+      <h1>지금 내게 필요한 대화주제 추천 서비스, F3P!</h1>
       <h2>
         나의 상황에 맞는 <br />
         카테고리를 골라보세요
       </h2>
       <div id="category">
         <button onClick={toRomantic} className="romantic">
-          연애
+          <span className="title">
+            <span className="icon">❤️</span>커플
+          </span>
           <br />
-          <span>❤️</span>
+          <span className="explanation">사랑을 묻고 답하다</span>
+          <img src="/img/heart-dynamic-clay.png" alt="Heart" />
         </button>
         <button onClick={toRelationship} className="relationship">
-          인간관계
+          <span className="title">
+            <span className="icon">😎</span>친구
+          </span>
           <br />
-          <span>👥</span>
+          <span className="explanation">밤새 끊이지 않는 대화</span>
+          <img src="img/thumb-up-dynamic-color.png" alt="Friend" />
         </button>
         <button onClick={toIcebreaking} className="icebreaking">
-          아이스브레이킹
+          <span className="title">
+            <span className="icon">🧊</span>아이스브레이킹
+          </span>
           <br />
-          <span>🗣️</span>
+          <span className="explanation">얼어붙은 분위기를 깨다</span>
+          <img src="img/chat-dynamic-gradient.png" alt="Friend" />
         </button>
       </div>
     </div>
