051628ce875382dbf98e7eb2246505a6a8e2b21a
sungyu0309 <tjsrb439@handong.ac.kr>
2024-01-31 00:24:09 +0900

    fix : 버튼 레이아웃 css 수정하여 올바르게 배열

--- src/components/SeriousConverBtn.js
+++ src/components/SeriousConverBtn.js
@@ -1,17 +1,22 @@
 import { useNavigate } from "react-router-dom";
+import styles from "./ConverHome.module.css";
 
 const SeriousConverBtn = () => {
   const movePage = useNavigate();
   const toSeriousConver = () => movePage("/category/relationship");
 
   return (
-    <button onClick={toSeriousConver} className="seriousConver">
+    <button onClick={toSeriousConver} className={styles.seriousConver}>
       <span className="title">
         <span className="icon">💬</span>진지한대화
       </span>
       <br />
       <span className="explanation">우리 진지해져 볼까..?</span>
-      <img src="img/tea-cup-dynamic-premium.png" alt="Friend" />
+      <img
+        src="img/tea-cup-dynamic-premium.png"
+        alt="Friend"
+        className={styles.allBtnImg}
+      />
     </button>
   );
 };
